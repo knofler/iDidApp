397abc9ade76a30f2e62c6c7518532e01400f3d4
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-03-18 15:11:42 +1100

    api key added

--- server/config/local.env.js
+++ server/config/local.env.js
@@ -9,14 +9,14 @@ module.exports = {
   DOMAIN: 'http://localhost:9000',
   SESSION_SECRET: "serveme-secret",
 
-  FACEBOOK_ID: 'app-id',
-  FACEBOOK_SECRET: 'secret',
+  FACEBOOK_ID: '1580418822200809',
+  FACEBOOK_SECRET: '6c7418d383b205502fcfb8fe8fae443c',
 
-  TWITTER_ID: 'app-id',
-  TWITTER_SECRET: 'secret',
+  TWITTER_ID: 'ZccOO7Qs84J3YEcvuz4eU8otp',
+  TWITTER_SECRET: 's93zkajrAoh4Ns3p0dlLc9ZsQJOQrpsqE5lrIiFLEDDnC6Wwth',
 
-  GOOGLE_ID: 'app-id',
-  GOOGLE_SECRET: 'secret',
+  GOOGLE_ID: '899206423047-92p8u7nlnb7kcdras020mn6fjfe66ln0.apps.googleusercontent.com',
+  GOOGLE_SECRET: '6Qe8ZZYcWuxMKT0HH__kFCgQ',
 
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
