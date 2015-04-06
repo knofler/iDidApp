9727a1137ee9dacb4b09dcdce5298cbca4f3abdc
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-04-06 17:48:11 +1000

    image galary structure ready

--- server/api/upload/upload.model.js
+++ server/api/upload/upload.model.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var UploadSchema = new Schema({
-  name: String,
-  info: String,
+  img_name: String,
+  upload_date:Date,
   active: Boolean
 });
 
