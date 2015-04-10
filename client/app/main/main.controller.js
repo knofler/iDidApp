b7f3c5e20be0a8c7caf532fdc21416cde3dd2efe
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-04-11 01:35:27 +1000

    timeseries implemented

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,97 +3,45 @@
 angular.module('serveMeApp')
   .controller('MainCtrl', function ($scope,$http,socket) {
     
-   // Generate basic chart with static data by c3
-	$scope.bChart = c3.generate({
-	  bindto: '#bChart',
-	  data: {
-	  	columns: [
-	       ['data1', 30, 200, 100, 400, 150, 250],
-	       ['data2', 50, 20, 10, 40, 15, 25]
-	  	]
-	   }
-	 });
-
-	// Introduce additional axis for data2. Add data.axes and axis.y2.show as follows:
-	$scope.cChart = c3.generate({
-		bindto:'#cChart',
-		data:{
-			columns: [
-				['data1',30, 200, 100, 400, 150, 250],
-				['data2',50, 20, 10, 40, 15, 25]
-			],
-			axes:{
-				data2:'y2' //Add
-			},
-			types:{
-				data2:'bar' //Add
-			}
-		},
-		axis:{
-			y:{
-				label:{ //Add
-					text:'Y label',
-					position:'outer-middle'
-				},
-				tick:{
-					format:d3.format("$,") //ADD
-				}
-			},
-			y2:{
-				show:true,
-				label:{ //Add
-					text:'Y2 label',
-					position:'outer-middle'
-				}
-				
-			}
-		}
-	 });
-
 	// By using load() API, you can load data and update the chart dynamically as follows:
 	// *****************Generate chart without data********************
 	$scope.apiChart = c3.generate({
 		bindto:'#apiChart',
 		data:{
-			columns: [
-				['data1',30, 200, 100, 400, 150, 250],
-				['data2',50, 20, 10, 40, 15, 25]
-			],
-			axes:{
-				data2:'y2' //Add
-			},
-			types:{
-				data2:'bar' //Add
-			}
-		},
-		axis:{
-			y:{
-				label:{ //Add
-					text:'Y label',
-					position:'outer-middle'
-				},
-				tick:{
-					format:d3.format("$,") //ADD
-				}
-			},
-			y2:{
-				show:true,
-				label:{ //Add
-					text:'Y2 label',
-					position:'outer-middle'
-				}
-				
-			}
-		}
+		   x: 'x',
+				   columns: [
+		            ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+		//            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
+		            ['data1', 30, 200, 100, 400, 150, 250],
+		            ['data2', 130, 340, 200, 500, 250, 350]
+		        ]
+    		},
+			axis: {
+			        x: {
+			            type: 'timeseries',
+			            tick: {
+			                format: '%Y-%m-%d'
+			            }
+			        }
+			    }
 	 });
+
+
+
+// setTimeout(function () {
+//     chart.load({
+//         columns: [
+//             ['data3', 400, 500, 450, 700, 600, 500]
+//         ]
+//     });
+// }, 1000);
 
 	// ************Load Data **************************
 	$scope.loadData = function(){
 	  $scope.apiChart.load({
-		columns:[
-		  ['data1',300, 100, 250, 150, 300, 150, 500],
-		  ['data2', 100, 200, 150, 50, 100, 250]
-		 ],
+		columns: [
+	            ['data3', 400, 500, 450, 700, 600, 500]
+	        ],
 		 types:{
 				data2:'line' //Add
 			}
